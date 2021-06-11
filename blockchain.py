5aa850a9020d9d07b6c3c689836e5c0bc1203bd7
ferueda <fe.rueda@outlook.com>
2021-06-11 14:30:03 -0400

    created proof_of_work method in Blockchain class

--- blockchain.py
+++ blockchain.py
@@ -18,3 +18,20 @@ class Blockchain:
 
     self.chain.append(block)
     return block
+
+  def get_previous_block(self):
+    return self.chain[-1]
+
+  def proof_of_work(self, prev_proof):
+    new_proof = 1
+    check_proof = False
+
+    while check_proof is False:
+      hash_operation = hashlib.sha256(str(new_proof**2 - prev_proof**2).encode()).hexdigest()
+
+      if hash_operation[:4] == '0000':
+        check_proof = True
+      else:
+        new_proof += 1
+      
+    return new_proof
